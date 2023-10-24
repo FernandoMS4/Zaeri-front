85da36d29f080505ad444dd1819277a432c4287c
Felipe Martins de Souza <felipemartins167@hotmail.com>
2023-10-24 19:04:25 -0300

    Login Route

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('Teste');
+  }
 
   ngOnInit(): void {
+    console.log('Teste');
   }
 
 }
